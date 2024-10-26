0089604680d0fb63c9114319f2cb92de28efb1af
Robert Zaremba <robert@zaremba.ch>
2024-10-26 14:58:59 +0200

    execute: new_affiliate

--- src/execute.rs
+++ src/execute.rs
@@ -5,16 +5,20 @@ use crate::error::ContractError;
 use crate::state::*;
 
 pub fn new_affiliate(deps: DepsMut, user: Addr, parent: Addr) -> Result<Response, ContractError> {
-    // let state = STATE.load(&deps.storage)?;
+    if AFF_PARENTS.has(deps.storage, user) {
+        return Err(ContractError::AlreadyAffiliated {});
+    }
 
     let state = STATE.update(deps.storage, |mut s: State| -> Result<_, ContractError> {
         s.next_aff_id += 1;
         Ok(s)
     })?;
 
+    AFF_PARENTS.save(deps.storage, user, &parent);
+
     Ok(Response::new()
         .add_attribute("action", "increment")
-        .add_attribute("affiliate_id", state.next_aff_id.into()))
+        .add_attribute("affiliate_id", state.next_aff_id))
 }
 
 pub fn distribute_rewards(
