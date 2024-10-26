6e975be31ade4b75038b819f1807fb5958efd420
Robert Zaremba <robert@zaremba.ch>
2024-10-26 07:34:44 +0200

    Initial Commit

---
+++ src/bin/schema.rs
@@ -0,0 +1,11 @@
+use cosmwasm_schema::write_api;
+
+use hackmos_affiliate::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+
+fn main() {
+    write_api! {
+        instantiate: InstantiateMsg,
+        execute: ExecuteMsg,
+        query: QueryMsg,
+    }
+}
